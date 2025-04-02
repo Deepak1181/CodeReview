6bae9e214c03c264009bd896201ac75e42b2cc44
DEEPAK PRAJAPATI <dkp041297@gmail.com>
2025-04-02 11:41:28 +0530

    vercel

--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -1,31 +1,5 @@
 
 
-
-
-
-// const aiService = require("../services/ai.service");
-
-// module.exports.getReview = async (req, res) => {
-//   try {
-//     console.log("Received body:", req.body); // Debugging
-
-//     const { code } = req.body.code;
-
-//     if (!code) {
-//       return res.status(400).json({ error: "Prompt   or code is required" });
-//     }
-
-    
-//     const response = await aiService.generateResponse(code);
-
-//     res.json({ response });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
-
 const aiService = require("../services/ai.service")
 
 module.exports.getReview = async (req, res) => {
